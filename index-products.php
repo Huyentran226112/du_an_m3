d189cdf1a3ede95444d0aa494648a69e1c8b5e52
Huyentran226112 <huyentran221100@gmail.com>
2023-07-12 09:21:01 +0700

    đã xong

--- index-products.php
+++ index-products.php
@@ -13,7 +13,7 @@ $products = $stmt->fetchAll();
             <div class="col-lg-3 col-md-4 col-sm-6 pb-1">
                 <div class="product-item bg-light mb-4">
                     <div class="product-img position-relative overflow-hidden">
-                        <img class="img-fluid w-100" src="admin<?php echo $row['image'];?>" alt="">
+                        <img class="img-fluid w-100" src="admin/<?php echo $row['image'];?>" alt="">
                         <div class="product-action">
                             <a class="btn btn-outline-dark btn-square" href=""><i class="fa fa-shopping-cart"></i></a>
                             <a class="btn btn-outline-dark btn-square" href=""><i class="fa fa-search"></i></a>
